c7ea3b21ad06761147c021dfbbb66f5d9136f3e2
vidhii015 <vidhivekariya015@gmail.com>
2024-12-14 21:35:33 +0530

    updated

--- JavaScript/header.js
+++ JavaScript/header.js
@@ -1,6 +1,8 @@
 let sidebar = document.getElementById("sidebar");
 let nav = document.getElementsByTagName("nav")[0];
 
+console.log("loaded");
+
 sidebar.addEventListener("click", () => {
   nav.classList.toggle("show");
 });
