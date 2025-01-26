1f26fbdf6a34c1d6800d48bd6effe30c48e2085a
vidhii015 <vidhivekariya015@gmail.com>
2025-01-26 15:21:59 +0530

    fix header

--- JavaScript/home.js
+++ JavaScript/home.js
@@ -7,6 +7,8 @@ const cartBtns = document.querySelectorAll(
 
 cartBtns.forEach((cartBtn) => {
   cartBtn.addEventListener("click", (e) => {
+    cartBtn.innerHTML = `<i class="fa-solid fa-check"></i>added to cart`; //  Changing button's text
+
     let item = e.target.closest(".product-item").cloneNode(true); // Clone the entire product-item
     addToCart(item); // Pass the cloned item to addToCart
   });
